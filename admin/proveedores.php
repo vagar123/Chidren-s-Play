a2d38721003121a87f0af799dc8e9436a3ec0ba5
Diego Lopez <diegolopez991108@gmail.com>
2020-11-08 23:07:21 -0500

    Se agrega estilo a Proveedores

--- admin/proveedores.php
+++ admin/proveedores.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <title>formulario</title>
     <link
@@ -10,98 +10,99 @@
     />
     <link rel="stylesheet" href="./Css/proveedores.css">
   </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#"
-        ><img src="../img/logoH.png" alt="logo" width="150"
-      /></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#" style="color: white">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Inventario</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Proveedores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Usuarios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Ventas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Mi cuenta</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Salir</a>
-          </li>
-        </ul>
-      </div>
+
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#"><img src="../img/logoH.png" alt="logo" width="150"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#" style="color:white;">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inventario</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Proveedores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ventas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mi cuenta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Salir</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
-    <center>
-      <form action="proveedores.php" method="POST">
+    <div class="container-fuid p-5">
         <div>
-          <h1>Formulario Proveedores</h1>
-          <label>id del Proveedor</label>
-          <input type="number" name="datos" placeholder="ID" />
+            <p>Diligencia el formulario para registrar un nuevo Proveedor en el sistema</p>
+        </div>
+      <form action="proveedores.php" method="POST">
 
-          <br /><br />
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <!-- <h1>Formulario Proveedores</h1> -->
+            <label>id del Proveedor</label>
+            <input class="form-control" type="number" name="datos" placeholder="ID" />
+          </div>
+         
+          <div class="form-group col-md-6">
+            <label>Nombre Proveedores</label>
+            <input class="form-control" type="text" name="nombreP" placeholder="Nombre Proveedores" />
+          </div>
+        </div>
 
-          <label>Nombre Proveedores</label>
-          <input type="text" name="nombreP" placeholder="Nombre Proveedores" />
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Correo Proveedores</label>
+            <input class="form-control" type="email" name="correo" placeholder="Correo Proveedores" />
+          </div>
 
-          <br /><br />
+          <div class="form-group col-md-6">
+            <label>Telefono Proveedores</label>
+            <input class="form-control"
+              type="number"
+              name="telfo"
+              placeholder="Telefono Proveedores"
+            />
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Dirrecciones Proveedores</label>
+            <input class="form-control"
+              type="text"
+              name="direcc"
+              placeholder="Dirrecciones Proveedores"
+            />
+          </div>
 
-          <label>Correo Proveedores</label>
-          <input type="email" name="correo" placeholder="Correo Proveedores" />
+          <div class="form-group col-md-6">
+            <label>Fecha</label>
+            <input  class="form-control" type="date" name="fecha" placeholder="Fecha" />
+          </div>
+        </div>
 
-          <br /><br />
-
-          <label>Telefono Proveedores</label>
-          <input
-            type="number"
-            name="telfo"
-            placeholder="Telefono Proveedores"
-          />
-
-          <br /><br />
-
-          <label>Dirrecciones Proveedores</label>
-          <input
-            type="text"
-            name="direcc"
-            placeholder="Dirrecciones Proveedores"
-          />
-
-          <br /><br />
-
-          <label>Fecha</label>
-          <input type="date" name="fecha" placeholder="Fecha" />
-
-          <br /><br />
-          <input type="submit" value="Enviar" name="btn1" />
+        <button type="submit" class="btn btn-primary" name="boton">Registrar</button>
         </div>
       </form>
-    </center>
 
     <?php 
-    if(isset($_POST['btn1'])){
+    if(isset($_POST['boton'])){
     include("../abrir_conexion.php");
 
     $datos=$_POST['datos'];
@@ -116,6 +117,13 @@
     values('$datos','$nombreP','$correo','$telfo','$direcc','$fecha')");
     echo"los datos fueron ingresados correctamente";
     include("../cerrar_conexion.php"); } ?>
+
+    <footer class="page-footer font-small blue" style="border-top: 1px solid rgb(226, 226, 226);">
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="#"> Children's Play.com</a>
+        </div>
+    </footer>
+
     <script
       src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
       integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
